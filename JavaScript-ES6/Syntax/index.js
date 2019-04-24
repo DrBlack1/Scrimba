4f13799f8651182002cb9d34a442f00e2b8eee28
yourUserName <patrick.mack1@yahoo.com>
2019-04-24 21:45:59 +0100

    Create index.js

---
+++ JavaScript-ES6/Syntax/index.js
@@ -0,0 +1,22 @@
+class SpaceShuttle {
+    constructor(targetPlanet){
+        this.targetPlanet = targetPlanet;
+    }
+}
+let zeus = new SpaceShuttle('Jupiter');
+
+console.log(zeus.targetPlanet)
+
+
+
+function makeClass() {
+    class Vegetable {
+        constructor(name){
+            this.name = name;
+        }
+    }
+    return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
