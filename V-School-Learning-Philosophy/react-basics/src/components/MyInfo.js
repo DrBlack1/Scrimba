c1775a8c00b657fea324f1721469ff9eb57c5b8b
yourUserName <patrick.mack1@yahoo.com>
2019-04-24 16:05:20 +0100

    Added react-basics

---
+++ V-School-Learning-Philosophy/react-basics/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Patrick Mack</h1>
+            <p>This is a paragraph about me...</p>
+            <ul>
+                <li>Football</li>
+                <li>Paintballing</li>
+                <li>Clay Shooting</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo;
