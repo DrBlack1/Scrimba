9d6c61778e0d6a79419d7aee89597e1e28a1ca09
yourUserName <patrick.mack1@yahoo.com>
2019-04-25 22:17:02 +0100

    Added multi functions

---
+++ JavaScript/Functions/index.js
@@ -0,0 +1,36 @@
+function add() {
+    console.log('add');
+}
+
+add();
+add();
+add();
+add();
+
+
+
+function add() {
+    return 5;
+}
+
+console.log(add());
+
+
+
+function add(num1) {
+    return num1;
+}
+
+console.log(add(10));
+console.log(add(15));
+console.log(add(20));
+
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(10, 6));
+console.log(add(15, 7));
+console.log(add(20,2));
