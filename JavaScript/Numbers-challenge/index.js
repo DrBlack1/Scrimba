f11e5b06c89c889f4680d461722bdeee37538abd
yourUserName <patrick.mack1@yahoo.com>
2019-04-25 18:01:47 +0100

    Updated numbers

---
+++ JavaScript/Numbers-challenge/index.js
@@ -0,0 +1,20 @@
+let example1 = parseInt("33 World 22");
+let example2 = parseFloat('44 Patrick 33');
+let example3 = 55.3333.toFixed(0);
+let example4 = 200.0.toFixed(2);
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
+
+
+let example1 = parseInt("33 World 22");
+let example2 = parseFloat('44 Patrick 33');
+let example3 = 55.3333.toFixed(0);
+let example4 = 200.0.toFixed(2);
+
+console.log(typeof example1);
+console.log(typeof example2);
+console.log(typeof example3);
+console.log(typeof example4);
