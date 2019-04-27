6c73a41ada1a76dbf0b097a441509c3457b9c7c4
yourUserName <patrick.mack1@yahoo.com>
2019-04-27 19:23:14 +0100

    Updated html

---
+++ HTML5/Forms-&-Inputs/index.js
@@ -0,0 +1,8 @@
+window.toggleLeft = function() {
+    let element = document.getElementById("offcanvas-left");
+    element.classList.toggle("hide");
+}
+window.toggleRight = function() {
+    let element = document.getElementById("offcanvas-right");
+    element.classList.toggle("hide");
+}
