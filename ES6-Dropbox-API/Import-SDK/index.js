a30711e4ada641855e68a5016af14429983e37ca
yourUserName <patrick.mack1@yahoo.com>
2019-05-01 23:04:01 +0100

    Added files

---
+++ ES6-Dropbox-API/Import-SDK/index.js
@@ -0,0 +1,8 @@
+// dropbox.com/developer
+
+import { Dropbox } from 'dropbox'
+
+const dbx = new Dropbox({
+    accessToken 'aeOL1E1H0AAAAAAAAAALX6z1ogWy75HGE_HBN-NNpJNfhnEa1kjF1vsJ_t7Wf8k',
+    fetch
+})
