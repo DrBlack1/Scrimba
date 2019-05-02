ee7de26c610c8daf00a9f931854bc018a95e1169
yourUserName <patrick.mack1@yahoo.com>
2019-05-02 23:19:30 +0100

    Added interfaces

---
+++ TypeScript/Interfaces/index.ts
@@ -0,0 +1,7 @@
+import { Person } from './person.interface';
+
+const example1: Person = {firstName: 'Patto', middleName: 'Patto', lastName: 'Patto'};
+
+example1.firstName = 'Patto';
+example1.middleName = 'Coding God';
+example1.lastName = 'Mack';
