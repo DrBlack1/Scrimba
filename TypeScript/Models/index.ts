a01cf16d8b6fb0230127fcb1c977b62d20e28c3a
yourUserName <patrick.mack1@yahoo.com>
2019-05-02 23:20:08 +0100

    Added models

---
+++ TypeScript/Models/index.ts
@@ -0,0 +1,7 @@
+import { Person } from './person.model';
+
+const example1: Person = new Person({firstName: 'Patto'});
+
+example1.firstName = 'Patto';
+example1.middleName = 'Coding God';
+example1.lastName = 'Mack';
