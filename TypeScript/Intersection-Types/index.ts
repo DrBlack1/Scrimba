6b1d75e10a335f38943aa828f22544175e5aae64
yourUserName <patrick.mack1@yahoo.com>
2019-05-02 23:20:30 +0100

    Updated interface

---
+++ TypeScript/Intersection-Types/index.ts
@@ -0,0 +1,12 @@
+import { Bear, Man, Pig } from './interfaces';
+
+let man: Man | Pig;
+let bear: Bear;
+let pig: Pig;
+
+type ManBearPig = Bear & Man & Pig
+
+let manBearPig: ManBearPig ;
+manBearPig.firstName = 'Patto';
+manBearPig.claws = 3;
+manBearPig.bacon = false;
