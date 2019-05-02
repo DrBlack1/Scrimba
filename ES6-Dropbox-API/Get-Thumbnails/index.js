7b0406f68a0994d574d6e08079956cfbdd955346
yourUserName <patrick.mack1@yahoo.com>
2019-05-02 18:34:23 +0100

    Updated thumbnails

---
+++ ES6-Dropbox-API/Get-Thumbnails/index.js
@@ -0,0 +1,85 @@
+// http://dropbox.github.io/dropbox-sdk-js/Dropbox.html
+
+import { Dropbox } from 'dropbox'
+
+const dbx = new Dropbox({
+    accessToken 'aeOL1E1H0AAAAAAAAAALX6z1ogWy75HGE_HBN-NNpJNfhnEa1kjF1vsJ_t7Wf8k',
+    fetch
+})
+
+const fileListElem = document.querySelector('.js-file-list')
+
+const state = {
+    files: []
+}
+
+const init = () => {
+    dbx.fileListFolder({
+        path: '',
+        limit: 20
+    }).then(res => {
+        updateFiles(res.entries)
+    })
+}
+
+const updateFiles = files => {
+    state.files = [...state.files, ...files]
+    renderFiles()
+    getThumbnails(files)
+}
+
+const renderFiles = () => {
+    fileListElem.innerHTML = state.files.sort((a, b) => {
+        // sort alphabetically, folders first
+        if ((a['.tag'] === 'folder' || b['tag'] === 'folder')
+        && !(a['tag'] === b['.tag'])) {
+            return a['.tag'] === 'folder' ? -1 : 1
+        } else {
+            return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+        }
+    }).map(file => {
+        const type = file['.tag']
+        let thumbnail
+        if (type === 'file') {
+            thumbnail = file.thumbnail
+            ? `data:image/jpeg;base64, ${file.thumbnail}`
+            : `data:image/svg+xml;base64,ashahssdhhjksjhshgdgakdbbsb+sbsnbsbsdhhdcnxnxnxbsnx`
+        } else {
+            thumbnail = `data:image/svg+xml;base64,jshshshdshjshhdjsjdjd+hshshsbsbsbsbbsbsnns`
+        }
+        return `
+        <li class="dbx-list-item ${type}">
+            <img class="dbx-thumb" src="${thumbnail}">
+            ${file.name}
+        </li>
+        `
+    }).join('')
+}
+
+const getThumbnails = files => {
+    const paths = files.filter(file => file['.tag'] === 'file')
+    .map(file => ({
+        path: file.path_lower,
+        size: 'w3h32'
+    }))
+    dbx.filesGetThumbnailBatch({
+        entries: paths
+    }).then(res => {
+        console.log(res)
+        // make a copy of state.files
+        const newStateFiles = [...state.files]
+        // loop through the file object returned from dbx
+        res.entries.forEach(file => {
+            // figure out the index of the file we need to update
+            let indexToUpdate = state.files.findIndex(
+                stateFile => file.metadata.path_lower === stateFile.path_lower
+            )
+            // put a .thumbnail property on the corresponding file
+            newStateFiles[indexToUpdate].thumbnail = file.thumbnail
+        })
+        state.files = newStateFiles
+        renderFiles()
+    })
+}
+
+init()
