066c08bf92c7a8afc664e23a6a74fa8b1bfcb55f
yourUserName <patrick.mack1@yahoo.com>
2019-05-02 21:39:30 +0100

    Added cursor

---
+++ ES6-Dropbox-API/Cursor/index.js
@@ -0,0 +1,80 @@
+// http://dropbox.github.io/dropbox-sdk-js/Dropbox.html
+
+// API exploer: https://dropbox.github.io/dropbox-api-v2-explorer/
+
+import { Dropbox } from 'dropbox'
+
+const dbx = new Dropbox({
+    accessToken 'aeOL1E1H0AAAAAAAAAALX6z1ogWy75HGE_HBN-NNpJNfhnEa1kjF1vsJ_t7Wf8k',
+    fetch
+})
+
+const fileListElem = document.querySelector('.js-file-list')
+
+const state = {
+    files: []
+}
+
+const init = async () => {
+    const res = await dbx.fileListFolder({
+        path: '',
+        limit: 5
+    })
+        updateFiles(res.entries)
+}
+
+const updateFiles = files => {
+    state.files = [...state.files, ...files]
+    renderFiles()
+    getThumbnails(files)
+}
+
+const renderFiles = () => {
+    fileListElem.innerHTML = state.files.sort((a, b) => {
+        // sort alphabetically, folders first
+        if ((a['.tag'] === 'folder' || b['tag'] === 'folder')
+        && !(a['tag'] === b['.tag'])) {
+            return a['.tag'] === 'folder' ? -1 : 1
+        } else {
+            return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+        }
+    }).map(file => {
+        const type = file['.tag']
+        let thumbnail
+        if (type === 'file') {
+            thumbnail = file.thumbnail
+            ? `data:image/jpeg;base64, ${file.thumbnail}`
+            : `data:image/svg+xml;base64,ashahssdhhjksjhshgdgakdbbsb+sbsnbsbsdhhdcnxnxnxbsnx`
+        } else {
+            thumbnail = `data:image/svg+xml;base64,jshshshdshjshhdjsjdjd+hshshsbsbsbsbbsbsnns`
+        }
+        return `
+        <li class="dbx-list-item ${type}">
+            <img class="dbx-thumb" src="${thumbnail}">
+            ${file.name}
+        </li>
+        `
+    }).join('')
+}
+
+const getThumbnails = async files => {
+    const paths = files.filter(file => file['.tag'] === 'file')
+    .map(file => ({
+        path: file.path_lower,
+        size: 'w3h32'
+    }))
+    const res = await dbx.filesGetThumbnailBatch({
+        entries: paths
+    })
+    const newStateFiles = [...state.files]
+    res.entries.forEach(file => {
+        let indexToUpdate = state.files.findIndex(
+            stateFile => file.metadata.path_lower === stateFile.path_lower
+        )
+        newStateFiles[indexToUpdate].thumbnail = file.thumbnail
+    })
+    state.files = newStateFiles
+    renderFiles()
+    }
+
+init()
