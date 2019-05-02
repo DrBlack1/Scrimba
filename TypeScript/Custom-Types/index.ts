4b1a0b9ead3b27a7f6cbb15699eab5d8c234c32a
yourUserName <patrick.mack1@yahoo.com>
2019-05-02 22:47:37 +0100

    Added types

---
+++ TypeScript/Custom-Types/index.ts
@@ -0,0 +1,9 @@
+type person = {firstName: string};
+
+const example1: object = undefined;
+
+const example2: Object = NaN;
+
+const example3: person = {firstName: 'Patto'};
+
+example3.firstName = 'Patto';
