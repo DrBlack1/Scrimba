276f0fe0131a4c110ef817bda9879b182aaa6b46
yourUserName <patrick.mack1@yahoo.com>
2019-05-03 21:47:56 +0100

    Added layouts

---
+++ Neural-Networks/Layers/index.js
@@ -0,0 +1,12 @@
+const net = new brain.NeuralNetwork({ hiddenLayers: [2, 2] });
+
+const trainingData = [
+    { input: [0, 0], output: [0] },
+    { input: [0, 1], output: [1] },
+    { input: [1, 0], output: [1] },
+    { input: [1, 1], output: [0] }
+];
+
+net.train(trainingData, {
+    log: (error) => console.log(error)
+});
