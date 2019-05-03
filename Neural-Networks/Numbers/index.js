0293ac0e95b68541a1660611237bbeb38ae5b668
yourUserName <patrick.mack1@yahoo.com>
2019-05-03 21:48:27 +0100

    Added numbers

---
+++ Neural-Networks/Numbers/index.js
@@ -0,0 +1,54 @@
+const restaurants = {
+    "Brilliant Yellow Corral": "Monday",
+    "Penny's": "Tuesday",
+    "Right Coast Wings": "Wednesday",
+    "The Delusion Last Railway Car": "Thursday",
+    "Fun Day Inn": "Friday",
+    "JHOP": "Saturday",
+    "Owls": "Sunday"
+};
+
+// input: { Monday, Tuesday, Wednesday, etx. }
+// output: { Restaurant1, Restaurant2 }
+
+const trainingData = [];
+
+for (let restaurantName in restaurants) {
+    const dayOfWeek = restaurants[restaurantName];
+    trainingData.push({
+        input: { [dayOfWeek]: 1 },
+        output: { [restaurantName]: 1 }
+    });
+}
+
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+const stats = net.train(trainingData);
+
+console.log(stats);
+
+console.log(net.run({ 'Monday': 1 }));
+
+function restaurantForDay(dayOfWeek) {
+    const result =net.run({ [dayOfWeek]: 1 });
+    let highestValue = 0;
+    let highestRestaurantName = '';
+    for (let restaurantName in result) {
+        if (result[restaurantName] > highestValue) {
+            highestValue = result[restaurantName];
+            highestRestaurantName = restaurantName;
+        } 
+    }
+
+    return highestRestaurantName;
+}
+
+console.log(restaurantForDay('Monday'));
+console.log(restaurantForDay('Tuesday'));
+console.log(restaurantForDay('Wednesday'));
+console.log(restaurantForDay('Thursday'));
+console.log(restaurantForDay('Friday'));
+console.log(restaurantForDay('Saturday'));
+console.log(restaurantForDay('Sunday'));
+
+// bonus
