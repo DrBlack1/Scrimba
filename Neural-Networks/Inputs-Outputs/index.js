11113867ddd02083c7e6bf266e7a4df7bc20fff0
yourUserName <patrick.mack1@yahoo.com>
2019-05-03 23:16:42 +0100

    Create index.js

---
+++ Neural-Networks/Inputs-Outputs/index.js
@@ -0,0 +1,16 @@
+const trainingData = [
+    { input: '1', output: '2' }
+];
+
+const net = new brain.recurrent.LSTM();
+
+const inputMap = ['1', 'NEW IDEA', '2'];
+
+[1,0,0]
+[0,1,0]
+[0,0,1]
+
+// { input: '2', output: '1' }
+[0,0,1]
+[0,1,0]
+[1,0,0]
