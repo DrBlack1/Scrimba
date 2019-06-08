3224dc4f095db413c5dd994867f7b306db101745
yourUserName <patrick.mack1@yahoo.com>
2019-06-08 15:29:43 +0100

    fix

---
+++ Angular/Application-Pt2/src/app/customers/customers.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { CustomerListComponet } from './customers-list/customer-list.component';
+import { FilterTextboxComponent } from './customers-list/filter-textbox.component';
+import { CustomersComponent } from './customers.component';
+import { CustomersRoutingModule } from './customers-routing.module';
+
+@NgModule({
+    imports: [ CommonModule, FormsModule, SharedModule, CustomersRoutingModule ],
+    declarations: [ CustomerListComponet, FilterTextboxComponent, CustomersComponent]
+})
+export class CustomersModule { }
